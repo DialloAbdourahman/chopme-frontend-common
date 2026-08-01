4c980523e392254cbdd806c608322d864b84152d
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2026-08-01 19:30:12 +0100

    fix

--- src/entities/restaurant-member.entity.ts
+++ src/entities/restaurant-member.entity.ts
@@ -1,8 +1,11 @@
 import { EnumRestaurantMemberRole } from "../enums/restaurant-member-role";
+import { IRestaurantEntity } from "./restaurant.entity";
 import { IUserEntity } from "./user.entity";
 
 export interface IRestaurantMemberEntity {
   id: string;
+  restaurant: IRestaurantEntity;
+  user?: IUserEntity;
   role: EnumRestaurantMemberRole;
   createdAt: Date;
   updatedAt: Date;
