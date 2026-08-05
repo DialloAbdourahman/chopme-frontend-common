e0cd0ded05abe14328794570aaa1e99236ebf3f4
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2026-08-05 16:21:55 +0100

    fix

--- src/dto/restaurant-wallet.dto.ts
+++ src/dto/restaurant-wallet.dto.ts
@@ -1,23 +1,15 @@
 import { z } from "zod";
-import { EnumNetwork } from "../enums/networks";
 import { EnumWalletTypes } from "../enums/wallet-types";
 
-export const mobileWalletDataInputSchema = z.object({
-  network: z.nativeEnum(EnumNetwork),
+export const addRestaurantWalletSchema = z.object({
+  type: z.nativeEnum(EnumWalletTypes),
   number: z
     .string()
     .regex(
       /^\+2376\d{8}$/,
       "Phone number must be a valid Cameroonian number in the format +237620487789",
-    ),
+    )
+    .optional(),
 });
 
-export const addRestaurantWalletSchema = z.object({
-  type: z.nativeEnum(EnumWalletTypes),
-  mobileData: mobileWalletDataInputSchema.optional(),
-});
-
-export type MobileWalletDataInputDto = z.infer<
-  typeof mobileWalletDataInputSchema
->;
 export type AddRestaurantWalletDto = z.infer<typeof addRestaurantWalletSchema>;
