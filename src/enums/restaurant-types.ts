71f692c08b9a76da9a9af67a7fb8ed1053d780ae
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2026-09-10 16:13:32 +0100

    fixes

--- src/enums/restaurant-types.ts
+++ src/enums/restaurant-types.ts
@@ -1,4 +1,41 @@
 export enum EnumRestaurantType {
-  FAST_FOOD = 'FAST_FOOD',
-  CAFE = 'CAFE',
+  // General
+  RESTAURANT = "RESTAURANT",
+  FAST_FOOD = "FAST_FOOD",
+  SNACK = "SNACK",
+  CAFE = "CAFE",
+
+  // Cameroon / African
+  CAMEROONIAN = "CAMEROONIAN",
+  AFRICAN = "AFRICAN",
+  MAQUIS = "MAQUIS",
+  GARGOTE = "GARGOTE",
+  GRILL = "GRILL",
+  ROTISSERIE = "ROTISSERIE",
+
+  // Popular international
+  PIZZERIA = "PIZZERIA",
+  BURGER = "BURGER",
+  SHAWARMA = "SHAWARMA",
+  CHINESE = "CHINESE",
+  INDIAN = "INDIAN",
+  LEBANESE = "LEBANESE",
+  FRENCH = "FRENCH",
+  ITALIAN = "ITALIAN",
+
+  // Other common food businesses
+  BAKERY = "BAKERY",
+  DESSERT = "DESSERT",
+  JUICE_BAR = "JUICE_BAR",
+
+  // Drinks / nightlife
+  BAR = "BAR",
+  LOUNGE = "LOUNGE",
+
+  // Premium / accommodation
+  FINE_DINING = "FINE_DINING",
+  HOTEL_RESTAURANT = "HOTEL_RESTAURANT",
+
+  // Independent / home-based
+  HOME_COOK = "HOME_COOK",
 }
